b58a83bfad4d0e682de3486936ee5666c01f6afa
alexxdoug <115509422+alexxdoug@users.noreply.github.com>
2022-10-10 23:14:40 -0300

    Add files via upload

---
+++ Atividade5/Exercicio 5/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //pedir Medida em M
+        float m;
+        float cm;
+        Scanner S =new Scanner(System.in);
+        System.out.println("insira a medida em M");
+        m= S.nextFloat();
+        cm= m*100;
+        //Calcular m para cm (*100)
+        System.out.println(resultado da conversão 'm'-'cm' = " + cm) ;
+
+    }
+}
